acd4c282ca74311ed946993f7c8b48c15a0bc693
viictorcarvalho <you@example.com>
2022-06-10 23:19:11 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/zup/simcitysaojoao/ProdutosFragment.kt
@@ -0,0 +1,17 @@
+package br.com.zup.simcitysaojoao
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ProdutosFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_produtos, container, false)
+    }
+}
---
+++ app/src/main/java/br/com/zup/simcitysaojoao/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.simcitysaojoao.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.simcitysaojoao.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
